ca7f1dcb7931eb829676edddbc13ecda51a407e1
Sangram <sangram15.behera@gmail.com>
2022-07-04 02:29:09 +0530

    Update salary using ms test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using EmployeePayrollWithADO;
+using EmployeePayrollWithADO.Model.SalaryModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeePayrollWithADO
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSalaryDetails_AbleToUpdateSalaryDetails()
+        {
+            //Arrange
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                SalaryId = 2,
+                Month = "JAN",
+                EmployeeSalary = 0,
+                EmployeeId = 20
+            };
+
+            //Act
+            int EmpSalary = salary.UpdateEmployeeSalary(updateModel);
+
+            //Assert
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+}
